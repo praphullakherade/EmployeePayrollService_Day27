1e66cf5ad994db3ae2d1923f4c1df6100e15eedf
Praphulla Kherade <prafullkherade9@gmail.com>
2021-11-09 10:53:51 +0530

    Create employee payroll serice to read and write employee payroll to a console

---
+++ main/java/EmployeePayrollService.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class EmployeePayrollService {
+    public List<EmployeePayrollData> employeePayrollList;
+
+    public EmployeePayrollService() {
+        employeePayrollList = new ArrayList<>();
+    }
+
+    public static void main(String[] args) {
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+        Scanner consoleInputReader = new Scanner(System.in);
+        employeePayrollService.printWelcomeMessage();
+        employeePayrollService.readEmployeePayrollData(consoleInputReader);
+        employeePayrollService.writeEmployeePayrollData();
+    }
+
+    /* This method is implementing Welcome Message */
+    public void printWelcomeMessage() {
+        System.out.println("Welcome To The Employee Payroll Service System");
+    }
+
+    private void readEmployeePayrollData(Scanner consoleInputReader) {
+        System.out.println("Enter Employee ID : ");
+        int id = consoleInputReader.nextInt();
+        consoleInputReader.nextLine();
+        System.out.println("Enter Employee Name : ");
+        String name = consoleInputReader.nextLine();
+        System.out.println("Enter Employee Salary : ");
+        double salary = consoleInputReader.nextDouble();
+        employeePayrollList.add(new EmployeePayrollData(id, name, salary));
+    }
+
+    private void writeEmployeePayrollData() {
+        System.out.println("\nWriting Employee Payroll Roaster to console\n" + employeePayrollList);
+    }
+}
